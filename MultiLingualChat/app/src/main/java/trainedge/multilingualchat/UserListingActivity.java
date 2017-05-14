5ea017a14cff8fd76cb53c962ffa5c7ab99ad189
zaid <xaidemetamorphos@gmail.com>
2017-05-14 15:49:23 +0530

    Looks complete 2

--- MultiLingualChat/app/src/main/java/trainedge/multilingualchat/UserListingActivity.java
+++ MultiLingualChat/app/src/main/java/trainedge/multilingualchat/UserListingActivity.java
@@ -3,6 +3,7 @@ package trainedge.multilingualchat;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -17,6 +18,8 @@ import trainedge.multilingualchat.adapters.UserListingPagerAdapter;
 import trainedge.multilingualchat.core.logout.LogoutContract;
 import trainedge.multilingualchat.core.logout.LogoutPresenter;
 
+import static trainedge.multilingualchat.SettingActivity.THEME;
+
 
 public class UserListingActivity extends AppCompatActivity implements LogoutContract.View {
     private Toolbar mToolbar;
@@ -24,6 +27,7 @@ public class UserListingActivity extends AppCompatActivity implements LogoutCont
     private ViewPager mViewPagerUserListing;
 
     private LogoutPresenter mLogoutPresenter;
+    private SharedPreferences app_pref;
 
     public static void startActivity(Context context) {
         Intent intent = new Intent(context, UserListingActivity.class);
@@ -39,6 +43,13 @@ public class UserListingActivity extends AppCompatActivity implements LogoutCont
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        app_pref = getSharedPreferences("app_pref", MODE_PRIVATE);
+        String theme = app_pref.getString(THEME, "light");
+        if (theme.equals("dark")) {
+            setTheme(R.style.AppThemeDark);
+        } else if (theme.equals("Black")) {
+            setTheme(R.style.AppTheme);
+        }
         setContentView(R.layout.activity_user_listing);
         bindViews();
         init();
